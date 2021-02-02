360e82433efe2fff04bb5c12e71d1f528cc2a36d
yocojean <yocojean@etu.uca.fr>
2021-02-02 09:49:18 +0100

    [DEV] Début du Menu Principal

--- app/src/main/java/iut/projet/view/ActivitePrincipale.java
+++ app/src/main/java/iut/projet/view/ActivitePrincipale.java
@@ -1,16 +1,16 @@
-package iut.cours.tp.view;
+package iut.projet.view;
 
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import iut.cours.tp.R;
+import iut.projet.R;
 
 public class ActivitePrincipale extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activite_principale);
     }
 }
+
