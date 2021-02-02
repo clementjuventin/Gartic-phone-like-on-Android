c450bdb13573f83de603aae5de510eeb9bd228e7
yocojean <yocojean@etu.uca.fr>
2021-02-02 08:32:42 +0100

    [DEV] Mise en place du Projet

---
+++ app/src/main/java/iut/projet/view/ActivitePrincipale.java
@@ -0,0 +1,16 @@
+package iut.cours.tp.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import iut.cours.tp.R;
+
+public class ActivitePrincipale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
