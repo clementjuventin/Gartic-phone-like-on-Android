a9a81634713d02f42172203b0e697cff142e7d5e
clementjuventin <62065633+clementjuventin@users.noreply.github.com>
2021-02-25 10:58:14 +0100

    --

---
+++ app/src/main/java/iut/projet/metier/FirebaseHelper.java
@@ -0,0 +1,12 @@
+package iut.projet.metier;
+
+import com.google.android.gms.tasks.Task;
+
+public class FirebaseHelper {
+    Firebase FirebaseStorage storage;
+    StorageReference storageReference;
+
+    public static void createImageDisplay(String urlImage, String message, Player sender){
+
+    }
+}
