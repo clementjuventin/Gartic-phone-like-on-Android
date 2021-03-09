d40fe659080a12db84300f96a4ce9200e80baf87
yocojean <yocojean@etu.uca.fr>
2021-03-09 15:02:56 +0100

    [DEV] rules

--- app/src/main/java/iut/projet/view/RulesActivity.java
+++ app/src/main/java/iut/projet/view/RulesActivity.java
@@ -2,10 +2,13 @@ package iut.projet.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -19,6 +22,10 @@ import iut.projet.metier.Player;
 public class RulesActivity extends AppCompatActivity implements View.OnClickListener {
     Context ctxt=this;
     ImageButton btnBack;
+    Button btnPageBack;
+    Button btnPageNext;
+    ImageView img;
+    Integer cpt=1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,13 +33,44 @@ public class RulesActivity extends AppCompatActivity implements View.OnClickList
         this.setContentView(R.layout.rules_activity);
 
         btnBack=findViewById(R.id.rules_retour);
+        btnPageBack=findViewById(R.id.rules_page_retour);
+        btnPageNext=findViewById(R.id.rules_page_next);
         btnBack.setOnClickListener(this);
+        btnPageBack.setOnClickListener(this);
+        btnPageNext.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.rules_retour) {
             finish();
+        }
+        img = findViewById(R.id.ruleImg);
+        if (v.getId() == R.id.rules_page_retour) {
+            if (cpt != 1) {
+                if (cpt == 2) {
+                    Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.rule1);
+                    img.setImageBitmap(bMap);
+                }
+                if (cpt == 3) {
+                    Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.rule2);
+                    img.setImageBitmap(bMap);
+                }
+                cpt--;
+            }
+        }
+        if (v.getId() == R.id.rules_page_next) {
+            if (cpt != 3) {
+                if (cpt == 1) {
+                    Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.rule2);
+                    img.setImageBitmap(bMap);
+                }
+                if (cpt == 2) {
+                    Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.rule3);
+                    img.setImageBitmap(bMap);
+                }
+                cpt++;
+            }
         }
     }
 }
